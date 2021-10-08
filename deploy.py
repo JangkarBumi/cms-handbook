e88ff91189831a573987719ce0abd57313afedf3
Zeha Irawan <zehairawan@gmail.com>
2021-10-08 09:24:11 +0800

    update py.

--- deploy.py
+++ deploy.py
@@ -1,7 +1,6 @@
-import  git
+import git
 
 repo = git.Repo('.')
 repo.git.add('--all')
-repo.git.commit('-m', 'commit message from python script')
 origin = repo.remote(name='origin')
 origin.push()
