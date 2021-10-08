bb26ff4d731cdb21db42e709cc0eb06c195b2a11
Zeha Irawan <zehairawan@gmail.com>
2021-10-08 09:23:38 +0800

    commit message from python script

--- deploy.py
+++ deploy.py
@@ -1,11 +1,7 @@
-from git import Repo, remote
+import  git
 
-rw_dir = 'path/to/your/local/repo'
-repo = Repo(rw_dir)
-
-'''Enter code to commit the repository here.
-After commit run the following code to push the commit to remote repo.
-I am pushing to master branch here'''
-
+repo = git.Repo('.')
+repo.git.add('--all')
+repo.git.commit('-m', 'commit message from python script')
 origin = repo.remote(name='origin')
 origin.push()
