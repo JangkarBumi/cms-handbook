309be539f8160a7218e6314fc6996b73dfd3d3ca
Zeha Irawan <zehairawan@gmail.com>
2021-10-12 15:58:24 +0800

    comment from python script

--- deploy.py
+++ deploy.py
@@ -1,10 +1,27 @@
-import git
+from git import Repo
 
-repo = git.Repo('.')
-repo.git.add('--all')
-repo.git.commit('-m', 'commit message from python script')
-origin = repo.remote(name='origin')
-origin.push()
+PATH_OF_GIT_REPO = r'.'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'comment from python script'
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')
+
+git_push()
+
+# import git
+
+# repo = git.Repo('.')
+# repo.git.add('--all')
+# repo.git.commit('-m', 'commit message from python script')
+# origin = repo.remote(name='origin')
+# origin.push()
 
 # os.chdir('./align')
 # Get the current working directory
